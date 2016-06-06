816facb78c38edd7d85039dbb9fb7f29a51a827c
huygamer <nguyenngoch@gmail.com>
2016-06-06 10:03:56 +0700

    Initial Commit

---
+++ LKMoleGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKMoleGame
+//
+//  Created by Huy Nguyen on 6/6/16.
+//  Copyright © 2016 Huy Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKMoleGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  LKMoleGame
+//
+
+//  Copyright (c) 2016 Huy Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
